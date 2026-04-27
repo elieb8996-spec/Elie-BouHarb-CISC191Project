3c88468c5abc89448c4fa257fef92ffa41ae4fe8
elieb8996-spec <elieb8996@gmail.com>
2026-04-27 14:49:31 -0700

    Update FloorTile.java

--- src/FloorTile.java
+++ src/FloorTile.java
@@ -11,15 +11,38 @@
  * Retrieved from https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
  * Oracle. “Method Overriding.” Java Platform SE Documentation.
  * https://docs.oracle.com/javase/tutorial/java/IandI/override.html
- * Version/date: 04-20-2026
+ * Oracle. (n.d.). Graphics (Java Platform SE Documentation).
+ * Retrieved from https://docs.oracle.com/javase/8/docs/api/java/awt/Graphics.html
+ * Oracle. BufferedImage. Java Platform SE 8 Documentation
+ * https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html
+ * Version/date: 04-27-2026
  * 
  * Responsibilities of class:
  * 
  */
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ * FloorTile IS-A Tile
+ * FloorTile HAS-A BufferedImage
+ */
 public class FloorTile extends Tile {
 
+    private BufferedImage image;
+
+    public FloorTile(BufferedImage image) {
+        this.image = image;
+    }
+
     @Override
-    public String getSymbol() {
-        return ".";
+    public void onEnter(Player player) {
+        // Nothing happens
+    }
+
+    @Override
+    public void draw(Graphics g, int x, int y, int size) {
+        if (image != null)
+            g.drawImage(image, x, y, size, size, null);
     }
 }
