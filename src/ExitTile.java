b03013c75e35ad4e1f826a0d135927a24bdfb150
elieb8996-spec <elieb8996@gmail.com>
2026-04-27 14:41:09 -0700

    Update ExitTile.java

--- src/ExitTile.java
+++ src/ExitTile.java
@@ -14,20 +14,37 @@
  *
  * Oracle. “Method Overriding.” Java Platform SE Documentation.
  * https://docs.oracle.com/javase/tutorial/java/IandI/override.html
- * Version/date: 04-20-2026
- * 
- * Responsibilities of class:
- * 
+ * Oracle. (n.d.). Graphics (Java Platform SE Documentation).
+ * Retrieved from https://docs.oracle.com/javase/8/docs/api/java/awt/Graphics.html
+ * Oracle. BufferedImage. Java Platform SE 8 Documentation
+ * https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html
+ *Version/date: 04-20-2026
+ *Responsibilities of class:
+ *
+ */
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ * ExitTile IS-A Tile
+ * ExitTile HAS-A BufferedImage
  */
 public class ExitTile extends Tile {
 
-    @Override
-    public boolean isExit() {
-        return true;
+    private BufferedImage image;
+
+    public ExitTile(BufferedImage image) {
+        this.image = image;
     }
 
     @Override
-    public String getSymbol() {
-        return "E";
+    public void onEnter(Player player) {
+        // Win logic handled in controller
+    }
+
+    @Override
+    public void draw(Graphics g, int x, int y, int size) {
+        if (image != null)
+            g.drawImage(image, x, y, size, size, null);
     }
 }
